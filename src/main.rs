ea7501b9acf4020910880c74474f14ff40ed986f
Chiro Liang <Chiro2001@163.com>
2024-03-19 23:47:31 +0800

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+//! CRC calculation
+
+#![deny(unsafe_code)]
+#![allow(clippy::empty_loop)]
+#![no_main]
+#![no_std]
+
+use panic_halt as _;
+
+use cortex_m_rt::entry;
+use cortex_m_semihosting::hprintln;
+use stm32f1xx_hal::{pac, prelude::*};
+
+#[entry]
+fn main() -> ! {
+    let p = pac::Peripherals::take().unwrap();
+
+    let mut crc = p.CRC.new();
+
+    crc.reset();
+    crc.write(0x12345678);
+
+    let val = crc.read();
+    hprintln!("found={:08x}, expected={:08x}", val, 0xdf8a8a2b_u32);
+
+    loop {}
+}
