ea5998d0bdf8b750bd30ece86d379c068ce28231
Chiro Liang <Chiro2001@163.com>
2024-03-25 05:04:43 +0800

    clean

--- src/main.rs
+++ src/main.rs
@@ -7,27 +7,19 @@
 extern crate alloc;
 
 use defmt::*;
-use embassy_futures::join::join;
-use embassy_usb::{
-    class::cdc_acm::{CdcAcmClass, State},
-    Builder,
+use embedded_graphics::prelude::*;
+use embedded_graphics::{
+    pixelcolor::Rgb565,
 };
-use embassy_usb_dfu::consts::DfuAttributes;
-use embassy_usb_driver::EndpointError;
-use embedded_graphics::pixelcolor::Rgb565;
 use {defmt_rtt as _, panic_probe as _};
 
-use embedded_graphics_core::prelude::*;
 use ili9341::{DisplaySize240x320, Ili9341, Orientation};
 
 use embassy_executor::Spawner;
 use embassy_stm32::{
-    bind_interrupts,
     gpio::{Flex, OutputType, Pull},
-    peripherals,
     time::Hertz,
     timer::{CaptureCompare16bitInstance, Channel},
-    usb::{self, Driver},
 };
 use embassy_stm32::{
     gpio::{Level, Output, Speed},
@@ -45,11 +37,6 @@ mod osc;
 use embedded_alloc::Heap;
 #[global_allocator]
 static HEAP: Heap = Heap::empty();
-
-bind_interrupts!(struct Irqs {
-    USB_LP_CAN1_RX0 => usb::InterruptHandler<peripherals::USB>;
-});
-
 struct DioPin<'d> {
     pin: Flex<'d>,
 }
@@ -243,83 +230,5 @@ async fn main(_spawner: Spawner) {
 
     let kbd = tm1668::TM1668::new(stb, clk, dio, &mut delay);
 
-    // Create the driver, from the HAL.
-    let driver = Driver::new(p.USB, Irqs, p.PA12, p.PA11);
-
-    // Create embassy-usb Config
-    let config = embassy_usb::Config::new(0xc0de, 0xcafe);
-    //config.max_packet_size_0 = 64;
-
-    // Create embassy-usb DeviceBuilder using the driver and config.
-    // It needs some buffers for building the descriptors.
-    let mut device_descriptor = [0; 256];
-    let mut config_descriptor = [0; 256];
-    let mut bos_descriptor = [0; 256];
-    let mut control_buf = [0; 7];
-
-    let mut state = State::new();
-
-    let mut builder = Builder::new(
-        driver,
-        config,
-        &mut device_descriptor,
-        &mut config_descriptor,
-        &mut bos_descriptor,
-        &mut [], // no msos descriptors
-        &mut control_buf,
-    );
-
-    // Create classes on the builder.
-    let mut class = CdcAcmClass::new(&mut builder, &mut state, 64);
-
-    // Build the builder.
-    let mut usb = builder.build();
-
-    // Run the USB device.
-    let usb_fut = usb.run();
-
-    // Do stuff with the class!
-    let echo_fut = async {
-        loop {
-            class.wait_connection().await;
-            info!("Connected");
-            let _ = echo(&mut class).await;
-            info!("Disconnected");
-        }
-    };
-
-    // Run everything concurrently.
-    // If we had made everything `'static` above instead, we could do this using separate tasks instead.
-    join(usb_fut, echo_fut).await;
-
-    // embassy_usb_dfu::usb_dfu(
-    //     &mut builder,
-    //     &mut embassy_usb_dfu::Control::new(
-    //         embassy_boot::BlockingFirmwareUpdater::new(),
-    //         DfuAttributes::CAN_UPLOAD | DfuAttributes::CAN_DOWNLOAD,
-    //     ),
-    // );
-}
-
-struct Disconnected {}
-
-impl From<EndpointError> for Disconnected {
-    fn from(val: EndpointError) -> Self {
-        match val {
-            EndpointError::BufferOverflow => defmt::panic!("Buffer overflow"),
-            EndpointError::Disabled => Disconnected {},
-        }
-    }
-}
-
-async fn echo<'d, T: usb::Instance + 'd>(
-    class: &mut CdcAcmClass<'d, Driver<'d, T>>,
-) -> Result<(), Disconnected> {
-    let mut buf = [0; 64];
-    loop {
-        let n = class.read_packet(&mut buf).await?;
-        let data = &buf[..n];
-        info!("data: {:x}", data);
-        class.write_packet(data).await?;
-    }
+    loop {}
 }
