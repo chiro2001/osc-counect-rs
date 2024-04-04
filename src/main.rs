9b585b3a201cd12464455ef87c338a2a11f76e3c
Chiro Liang <Chiro2001@163.com>
2024-04-04 11:11:07 +0800

    dma failed...

--- src/main.rs
+++ src/main.rs
@@ -16,11 +16,12 @@ use ili9341::{DisplaySize240x320, Ili9341 as Ili9327, Orientation};
 use embassy_executor::Spawner;
 use embassy_stm32::{
     bind_interrupts,
+    dma::Transfer,
     gpio::{Flex, OutputType, Pull},
     peripherals::ADC1,
     time::Hertz,
     timer::{CaptureCompare16bitInstance, Channel},
-    PeripheralRef,
+    Peripheral, PeripheralRef,
 };
 use embassy_stm32::{
     gpio::{Level, Output, Speed},
@@ -308,6 +309,132 @@ async fn main(spawner: Spawner) {
     let kbd_drv = KeyboardDriver::new(kbd);
 
     // spawner.spawn(app::main_loop(lcd, kbd_drv)).unwrap();
+
+    // pac::RCC.ahbenr().modify(|w| {
+    //     w.set_dma1en(true);
+    //     w.set_dma2en(true);
+    // });
+    // pac::RCC.apb2enr().modify(|w| {
+    //     w.set_adc1en(true);
+    //     w.set_adc2en(true);
+    // });
+
+    // let mut adc = embassy_stm32::adc::Adc::new(p.ADC1, &mut Delay);
+    // let mut pin = p.PA1;
+
+    // let mut vrefint = adc.enable_vref(&mut Delay);
+    // let vrefint_sample = adc.read(&mut vrefint).await;
+    // let convert_to_millivolts = |sample: u16| {
+    //     // From http://www.st.com/resource/en/datasheet/CD00161566.pdf
+    //     // 5.3.4 Embedded reference voltage
+    //     const VREFINT_MV: u32 = 1200; // mV
+
+    //     (u32::from(sample) * VREFINT_MV / u32::from(vrefint_sample)) as u16
+    // };
+    // let v = adc.read(&mut pin).await;
+    // info!("--> {} - {} mV", v, convert_to_millivolts(v));
+
+    // use pac::adc::regs::*;
+    // pac::ADC1.sr().write_value(Sr(0x00000010));
+    // pac::ADC1.cr1().write_value(Cr1(0x00000000));
+    // // ADC1, CR2, 0x001E0103, ADON: 0b1; CONT: 0b1; CAL: 0b0; RSTCAL: 0b0; DMA: 0b1; ALIGN: 0b0; JEXTSEL: 0b000; JEXTTRIG: 0b0; EXTSEL: 0b111; EXTTRIG: 0b1; JSWSTART: 0b0; SWSTART: 0b0; TSVREFE: 0b0
+    // // pac::ADC1.cr2().write_value(Cr2(0x001E0103));
+    // pac::ADC1.cr2().write(|w| {
+    //     w.set_adon(false);
+    //     w.set_cont(true);
+    //     w.set_cal(false);
+    //     w.set_rstcal(false);
+    //     w.set_dma(false);
+    //     w.set_align(false);
+    //     w.set_jextsel(0);
+    //     w.set_jexttrig(false);
+    //     w.set_extsel(0b111);
+    //     w.set_exttrig(true);
+    //     w.set_jswstart(false);
+    //     w.set_swstart(false);
+    //     w.set_tsvrefe(false);
+    // });
+    // pac::ADC1.smpr1().write_value(Smpr1(0x00000000));
+    // pac::ADC1.smpr2().write_value(Smpr2(0x00000000));
+    // pac::ADC1.htr().write_value(Htr(0x00000FFF));
+    // pac::ADC1.ltr().write_value(Ltr(0x00000000));
+    // pac::ADC1.sqr1().write_value(Sqr1(0x00000000));
+    // pac::ADC1.sqr2().write_value(Sqr2(0x00000000));
+    // pac::ADC1.sqr3().write_value(Sqr3(0x00000001));
+    // pac::ADC1.jsqr().write_value(Jsqr(0x00000000));
+
+    // // use continuous conversion mode
+    // // pac::ADC1.cr2().modify(|w| w.set_adon(false));
+    // // pac::ADC1.cr1().modify(|w| {
+    // //     w.set_scan(false);
+    // //     w.set_discen(false);
+    // // });
+    // // pac::ADC1.cr2().modify(|w| {
+    // //     w.set_cont(true);
+    // //     w.set_align(false);
+    // // });
+    // // pac::ADC1.cr2().modify(|w| w.set_adon(true));
+    // // pac::ADC1.sr().write_value(Sr(0x00000010));
+    // let mut values = [0u16; 12];
+    // // let mut options: embassy_stm32::dma::TransferOptions = Default::default();
+    // // options.circular = true;
+    // // options.half_transfer_ir = true;
+    // // options.priority = embassy_stm32::dma::Priority::High;
+    // // let transfer = unsafe {
+    // //     Transfer::new_read_raw(
+    // //         p.DMA1_CH1,
+    // //         (),
+    // //         pac::ADC1.dr().as_ptr() as *mut u16,
+    // //         &mut values as *mut [u16],
+    // //         options,
+    // //     )
+    // // };
+
+    // pac::ADC1.cr2().modify(|w| {
+    //     w.set_adon(true);
+    //     w.set_dma(true);
+    // });
+
+    // // DMA1, ISR, 0x00000007, GIF1: 0b1; TCIF1: 0b1; HTIF1: 0b1; TEIF1: 0b0; GIF2: 0b0; TCIF2: 0b0; HTIF2: 0b0; TEIF2: 0b0; GIF3: 0b0; TCIF3: 0b0; HTIF3: 0b0; TEIF3: 0b0; GIF4: 0b0; TCIF4: 0b0; HTIF4: 0b0; TEIF4: 0b0; GIF5: 0b0; TCIF5: 0b0; HTIF5: 0b0; TEIF5: 0b0; GIF6: 0b0; TCIF6: 0b0; HTIF6: 0b0; TEIF6: 0b0; GIF7: 0b0; TCIF7: 0b0; HTIF7: 0b0; TEIF7: 0b0
+    // // DMA1, IFCR, <Write-Only>, CGIF1: -; CTCIF1: -; CHTIF1: -; CTEIF1: -; CGIF2: -; CTCIF2: -; CHTIF2: -; CTEIF2: -; CGIF3: -; CTCIF3: -; CHTIF3: -; CTEIF3: -; CGIF4: -; CTCIF4: -; CHTIF4: -; CTEIF4: -; CGIF5: -; CTCIF5: -; CHTIF5: -; CTEIF5: -; CGIF6: -; CTCIF6: -; CHTIF6: -; CTEIF6: -; CGIF7: -; CTCIF7: -; CHTIF7: -; CTEIF7: -
+    // // DMA1, CCR1, 0x000025AF, EN: 0b1; TCIE: 0b1; HTIE: 0b1; TEIE: 0b1; DIR: 0b0; CIRC: 0b1; PINC: 0b0; MINC: 0b1; PSIZE: 0b01; MSIZE: 0b01; PL: 0b10; MEM2MEM: 0b0
+    // // DMA1, CNDTR1, 0x0000004B, NDT: 0b0000000001001011
+    // // DMA1, CPAR1, 0x4001244C, PA: 0b01000000000000010010010001001100
+    // // DMA1, CMAR1, 0x2000BF30, MA: 0b00100000000000001011111100110000
+
+    // pac::DMA1.ch(0).cr().write(|w| {
+    //     w.set_en(false);
+    //     w.set_tcie(true);
+    //     w.set_htie(true);
+    //     w.set_teie(true);
+    //     w.set_dir(pac::bdma::vals::Dir::FROMPERIPHERAL);
+    //     w.set_circ(true);
+    //     w.set_pinc(false);
+    //     w.set_minc(true);
+    //     w.set_psize(pac::bdma::vals::Size::BITS16);
+    //     w.set_msize(pac::bdma::vals::Size::BITS16);
+    //     w.set_pl(pac::bdma::vals::Pl::HIGH);
+    //     w.set_mem2mem(false);
+    // });
+    // pac::DMA1
+    //     .ch(0)
+    //     .ndtr()
+    //     .write(|w| w.set_ndt(values.len() as _));
+    // pac::DMA1.ch(0).par().write_value(0x4001244C);
+    // pac::DMA1
+    //     .ch(0)
+    //     .mar()
+    //     .write_value(&values as *const u16 as u32);
+    // pac::DMA1.ch(0).cr().modify(|w| w.set_en(true));
+    // info!("start continuous conversion");
+    // // transfer.await;
+    // // info!("done, values: {:?}", values);
+    // Timer::after_millis(1000).await;
+    // for v in values {
+    //     info!("value: {:x}", v);
+    // }
+
+    // loop {}
 
     app::main_loop(spawner, lcd, kbd_drv).await;
     defmt::panic!("unreachable");
