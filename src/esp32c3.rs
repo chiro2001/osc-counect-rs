26194bbdc9daab2683adef00f1a08208fbc7f37c
Chiro Liang <Chiro2001@163.com>
2024-04-12 23:35:17 +0800

    esp32c3 with st7789 blocking lcd

--- src/esp32c3.rs
+++ src/esp32c3.rs
@@ -1,27 +1,55 @@
 #![no_std]
 #![no_main]
 
+use core::cell::RefCell;
+
+use embassy_embedded_hal::shared_bus::blocking::spi::SpiDevice;
 use embassy_executor::Spawner;
+use embassy_sync::blocking_mutex::NoopMutex;
 use embassy_time::{Duration, Timer};
+use embedded_graphics::geometry::Size;
+use embedded_graphics::pixelcolor::{IntoStorage, Rgb565};
+use embedded_graphics::prelude::Primitive;
+use embedded_graphics::transform::Transform;
+use embedded_graphics::Drawable;
+use embedded_graphics::{
+    geometry::Point,
+    pixelcolor::RgbColor,
+    primitives::{PrimitiveStyle, Rectangle},
+};
 use esp_backtrace as _;
 use esp_hal::{
     clock::ClockControl,
-    embassy::{self},
+    delay::Delay,
+    embassy,
+    gpio::IO,
     peripherals::Peripherals,
     prelude::*,
+    spi::{master::Spi, SpiMode},
     timer::TimerGroup,
 };
 
-#[embassy_executor::task]
-async fn run() {
-    loop {
-        esp_println::println!("Hello world from embassy using esp-hal-async!");
-        Timer::after(Duration::from_millis(1_000)).await;
-    }
-}
+/*
+ST7789 <-> ESP32C3
+VCC    <-> 3.3V
+GND    <-> GND
+
+SCK    <-> IO2
+SDA    <-> IO3
+RES    <-> IO10
+DC     <-> IO6
+CS     <-> IO7
+BL     <-> IO11
+
+BTN0   <-> IO4
+BTN1   <-> IO5
+BTN2   <-> IO8
+BTN3   <-> IO9
+BTN    <-> IO13
+*/
 
 #[main]
-async fn main(spawner: Spawner) {
+async fn main(_spawner: Spawner) {
     esp_println::println!("Init!");
     let peripherals = Peripherals::take();
     let system = peripherals.SYSTEM.split();
@@ -30,10 +58,42 @@ async fn main(spawner: Spawner) {
     let timg0 = TimerGroup::new_async(peripherals.TIMG0, &clocks);
     embassy::init(&clocks, timg0);
 
-    spawner.spawn(run()).ok();
+    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+    let sclk = io.pins.gpio2;
+    let mosi = io.pins.gpio3;
+    let res = io.pins.gpio10.into_push_pull_output();
+    let dc = io.pins.gpio6.into_push_pull_output();
+    let cs = io.pins.gpio7.into_push_pull_output();
+    let bl = io.pins.gpio11.into_push_pull_output();
+
+    let spi = Spi::new(peripherals.SPI2, 100.kHz(), SpiMode::Mode0, &clocks)
+        .with_sck(sclk)
+        .with_mosi(mosi);
+    let spi_mutex = NoopMutex::new(RefCell::new(spi));
+    let spi_device = SpiDevice::new(&spi_mutex, cs);
+    let interface = display_interface_spi::SPIInterface::new(spi_device, dc);
+    let mut lcd = st7789::ST7789::new(interface, Some(res), Some(bl), 160, 80);
+    let mut delay = Delay::new(&clocks);
+    lcd.init(&mut delay).unwrap();
+    lcd.set_orientation(st7789::Orientation::Landscape).unwrap();
+
+    lcd.set_pixels(
+        0,
+        0,
+        160,
+        160,
+        core::iter::repeat(Rgb565::CYAN.into_storage()).take(160 * 160),
+    )
+    .unwrap();
+
+    Rectangle::new(Point::new(0, 0), Size::new(160, 80))
+        .into_styled(PrimitiveStyle::with_fill(Rgb565::GREEN))
+        .translate(Point::new(1, 26))
+        .draw(&mut lcd)
+        .unwrap();
 
     loop {
         esp_println::println!("Bing!");
-        Timer::after(Duration::from_millis(5_000)).await;
+        Timer::after(Duration::from_millis(1_000)).await;
     }
 }
